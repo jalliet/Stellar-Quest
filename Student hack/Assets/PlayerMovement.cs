167fe3011d8c8c464db3a9d46ad9df86922676dc
Jonathan <jon@Jonathans-MacBook-Pro-3.local>
2024-04-13 16:28:07 +0100

    Add player capsule and ground with movement

---
+++ Student hack/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public float maxSpeed = 5f; // Add this line at the top of your class
+    public float jumpForce = 1f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // rb.drag = 3;
+    }
+
+    void Update()
+    {
+
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+            isGrounded = false;
+            Debug.Log("jump");
+        }
+        
+    }
+    void LateUpdate(){
+        Debug.Log("move across");
+        float userInput = Input.GetAxisRaw("Horizontal");
+        rb.AddForce(new Vector3(userInput * speed, 0f, 0f), ForceMode.Force);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // Check if the collision object has the "Ground" tag
+        {
+            isGrounded = true;
+            Debug.Log("grounded");
+        }
+    }
+}
